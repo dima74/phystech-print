3452019cb62ad47898a302165654e689ea093337
Дмитрий Мурзин <dm1998@list.ru>
2017-04-22 20:26:40 +0300

    Чуть больше русского языка :)

--- src/instructions.py
+++ src/instructions.py
@@ -5,10 +5,10 @@ instructions = Blueprint('instructions', __name__)
 
 @instructions.route('/инструкции')
 def howto():
-    return render_template('instructions/index.html')
+    return render_template('инструкции/index.html')
 
 
-@instructions.route('/инструкции/faq')
-@instructions.route('/инструкции/refund')
+@instructions.route('/инструкции/часто_задаваемые_вопросы')
+@instructions.route('/инструкции/возврат_средств')
 def doc():
-    return render_template('{}.html'.format(request.path.replace('инструкции', 'instructions')))
+    return render_template(request.path + '.html')
