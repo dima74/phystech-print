8b0c8e31b7d5c2f3428b6449bf312d71e08d77b4
Дмитрий Мурзин <dm1998@list.ru>
2017-04-24 16:29:48 +0300

    Страницы из раздела /инструкции/часто_задаваемые_вопросы

--- src/instructions.py
+++ src/instructions.py
@@ -12,5 +12,10 @@ def howto():
 @instructions.route('/инструкции/возврат_средств')
 @instructions.route('/инструкции/настройка_драйвера')
 @instructions.route('/инструкции/ipp')
+@instructions.route('/инструкции/оплата_студенческой_карточкой')
+@instructions.route('/инструкции/двусторонняя_печать')
+@instructions.route('/инструкции/печать_без_драйверов')
+@instructions.route('/инструкции/удаление_документов_из_библиотеки')
+@instructions.route('/инструкции/изменение_свойств_порта')
 def doc():
     return render_template(request.path + '.html')
