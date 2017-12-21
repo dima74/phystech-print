7db7f59c648895c4af5740743899f768a0e58d0e
Дмитрий Мурзин <murzin.da@phystech.edu>
2017-12-21 13:12:04 +0300

    backup

--- BUILD.sh
+++ BUILD.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 set -x
 
 # cd && git clone git@github.com:dima74/phystech-print.git
@@ -10,5 +10,10 @@ systemctl enable gunicorn-phystech-print.service
 systemctl start gunicorn-phystech-print.service
 
 ln -s /home/dima/phystech-print/nginx /etc/nginx/phystech-print
+ln -s /etc/nginx/phystech-print/server-without-ssl.conf /etc/nginx/sites-enabled/phystech-print.conf
+nginx -s reload
+/home/dima/arch-install-digitalocean/nginx-ssl.sh xn----8sbnbhf6cvaflp1a1e.xn--p1ai
+
+rm /etc/nginx/sites-enabled/phystech-print.conf
 ln -s /etc/nginx/phystech-print/server.conf /etc/nginx/sites-enabled/phystech-print.conf
 nginx -s reload
