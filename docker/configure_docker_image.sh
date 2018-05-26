e5b7ea790872c594ec1e7702351732f64d6c2413
Дмитрий Мурзин <diraria@yandex.ru>
2018-05-26 21:42:01 +0300

    Fix UnicodeEncodeError

--- docker/configure_docker_image.sh
+++ docker/configure_docker_image.sh
@@ -1,11 +1,14 @@
 #!/bin/sh -e
 set -x
 
+sed -i '/ru_RU.UTF-8/s/^#//w /dev/stdout' /etc/locale.gen | read
+locale-gen
+echo LANG=ru_RU.UTF-8 >/etc/locale.conf
+export LANG=ru_RU.UTF-8
+
 pacman -Sy --noconfirm glibc  # bug with libnsl
 pacman -Sy --noconfirm python python-pip gunicorn nginx-mainline
 pip install --no-cache-dir -r `dirname $0`/../requirements.txt
 
 ln -s /app/nginx /etc/nginx/phystech-print
 cp /app/nginx/nginx.conf /etc/nginx/nginx.conf
-# sed -i '/http {/a include phystech-print\/server.conf;' /etc/nginx/nginx.conf
-# sed -i '/http {/i env PORT;' /etc/nginx/nginx.conf
